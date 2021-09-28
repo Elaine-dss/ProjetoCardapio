02bc85f4c09aa7a3212152349f8a75a38651e21f
Elaine <elaine.silva@zup.com.br>
2021-09-28 18:25:56 -0300

    "criando um metodo para cadastrar"

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,18 @@
 package br.com.zup;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Sistema {
+
+    private static Scanner tabelaCadastro(String msg) {
+        System.out.println(msg);
+        return new Scanner(System.in);
+    }
+
+    public static void Cadastrar() {
+        String nome = tabelaCadastro("|  Nome do prato: ").nextLine().toUpperCase(Locale.ROOT);
+        String ingredientes = tabelaCadastro("|  Igredientes: ").nextLine().toLowerCase(Locale.ROOT);
+        double valor = tabelaCadastro("|  Valor: ").nextDouble();
+    }
 }
